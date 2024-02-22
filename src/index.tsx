53b0cd3e7197049f4871c1f55b09ceddeeaa0927
dieuxuan1210 <xuandieu121094@gmail.com>
2024-02-22 15:49:30 +0700

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { HTMLDivProps, IProps } from '@uiw/utils';
+import './style/index.less';
+
+export interface ButtonGroupProps extends IProps, HTMLDivProps {
+  vertical?: boolean;
+}
+
+export default React.forwardRef<HTMLDivElement, ButtonGroupProps>((props, ref) => {
+  const { prefixCls = 'w-btn-group', vertical = false, children, className, ...resetProps } = props;
+
+  const cls = [prefixCls, className, vertical && `${prefixCls}-vertical`].filter(Boolean).join(' ').trim();
+
+  return (
+    <div className={cls} {...resetProps} ref={ref}>
+      {children}
+    </div>
+  );
+});
